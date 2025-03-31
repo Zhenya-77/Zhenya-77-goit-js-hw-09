bb8c197f8210256d37241258cdfdd4acac950559
Ваше Ім'я <zhekakrochak79@gmail.com>
2025-03-31 19:41:59 +0300

    midle

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,39 @@
-console.log('Gallery');
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const images = [
+  {
+    preview: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
+    description: 'Hokkaido Flower',
+  },
+  {
+    preview: 'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg',
+    description: 'Container Haulage Freight',
+  },
+  {
+    preview: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg',
+    original: 'https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg',
+    description: 'Aerial Beach View',
+  }
+];
+
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = createGalleryMarkup(images);
+
+function createGalleryMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery-item">
+          <a class="gallery-link" href="${original}">
+            <img class="gallery-image" src="${preview}" alt="${description}">
+          </a>
+        </li>
+      `;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
