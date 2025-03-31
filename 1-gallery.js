41b7c88c6a8250189f238bdc54c6ab7e18d98e53
Zhenya-77 <zhekakrochak79@gmail.com>
2025-03-31 16:47:48 +0000

    Deploying to gh-pages from @ Zhenya-77/Zhenya-77-goit-js-hw-09@5c1e8cf74ef3b420b19a25e364ab77f371860570 🚀

--- 1-gallery.js
+++ 1-gallery.js
@@ -1,2 +1,8 @@
-import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */console.log("Gallery");
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";import{S as t}from"./assets/vendor-CgTBfC_f.js";const r=[{preview:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",description:"Hokkaido Flower"},{preview:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",description:"Container Haulage Freight"},{preview:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",original:"https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",description:"Aerial Beach View"}],p=document.querySelector(".gallery");p.innerHTML=c(r);function c(a){return a.map(({preview:i,original:e,description:o})=>`
+        <li class="gallery-item">
+          <a class="gallery-link" href="${e}">
+            <img class="gallery-image" src="${i}" alt="${o}">
+          </a>
+        </li>
+      `).join("")}new t(".gallery a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=1-gallery.js.map
